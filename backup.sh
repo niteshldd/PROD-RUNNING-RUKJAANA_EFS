99ae21368a8e68f92cdaaf58af985ffb601346a2
ubu-prod <nitesh.ldd@gmail.com>
2019-10-02 16:26:22 +0530

    working with nginx 1.2

--- backup.sh
+++ backup.sh
@@ -1,3 +1,5 @@
 sftp -i rukja-prod.pem ec2-user@ec2-13-232-215-224.ap-south-1.compute.amazonaws.com
 size check - du -sh filename
 get -Pr Wordpress
+hidden file - ls -1a nginx | wc -l
+ls -1a wordpress | wc -l
